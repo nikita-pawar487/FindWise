99af963767804840b42b0bdd52202abcb3930ca5
nikita-pawar487 <nikitapawar01234@gmsil.com>
2026-07-26 11:13:04 +0530

    Added new products

--- assets/js/products.js
+++ assets/js/products.js
@@ -10,7 +10,7 @@ const products = [
     image: "assets/images/floating-shelf.png",
     description: "Beautiful home decor and organizer",
     review: "This shelf is lightweight, ergonomic, and has excellent  life. It's a great choice for students and professionals.",
-    affiliate: "https://link.amazon/B0hxTU96y?tag=homeorg1208-21"
+    affiliate: "https://www.amazon.in/BLUEWUD-Easton-Engineered-Display-Shelves/dp/B09T3D9K1Z?tag=homeorg12-21"
 },
 {
     id: 2,
@@ -19,7 +19,27 @@ const products = [
     image: "assets/images/kea-book.png",
     description: "Karnataka Village Administrative Officer Exam Book",
     review: "This book is comprehensive and well-structured, making it an excellent resource for exam preparation.",
-    affiliate: "https://link.amazon/B00I7S4sK?tag=homeorg1208-21"
+    affiliate: "https://www.amazon.in/2026-2027-Karnataka-Village-Administrative-Officer/dp/B0H8PFVHBZ?tag=homeorg12-21"
+},
+{
+    id: 3,
+    title: "Wooden Wall Shelf",
+    category: "Home",
+    image: "assets/images/wall-shelf.png",
+    description: "Stylish and functional wall shelf for your home",
+    review: "This shelf is well-built and adds a nice touch to any room.This stylish floating shelf is perfect for displaying plants, photo frames, and decorative items. Its modern design saves space and adds a premium touch to any room.",
+
+    affiliate: "https://www.amazon.in/Dime-Store-Engineered-Corner-Glossy/dp/B07RHZV825?tag=homeorg12-21"
+},
+{
+    id: 4,
+    title: "Book Shelf",
+    category: "Home",
+    image: "assets/images/book-shelf.png",
+    description: "Stylish bookshelf for your home",
+    review: "This bookshelf is well-built and adds a nice touch to any room.This wooden desk organizer is a practical solution for keeping books, stationery, and small accessories neatly arranged. Its modern design saves space while adding a clean and organized look to your study or workspace.",       
+        
+    affiliate: "https://www.amazon.in/Enigmatic-Woodworks-Bookshelves-Organizer-Accessories/dp/B0FD7N7GZ6?tag=homeorg12-21"
 },
 
 ];
